0daaff5de9fa075c72b74e1154009bc78ccdc40f
Clinton Bosch <clinton.bosch@gmail.com>
2025-08-14 09:53:07 +0200

    Use SLF4J for structured logging

--- src/main/java/za/co/prescient/Utils.java
+++ src/main/java/za/co/prescient/Utils.java
@@ -3,20 +3,23 @@ package za.co.prescient;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Utils {
+    private static final Logger logger = LoggerFactory.getLogger(Utils.class);
 
     public static void process(SNSEvent.SNSRecord record) {
         String message = record.getSNS().getMessage();
-        System.out.println("Processing: " + message);
+        logger.info("Processing SNS message: {}", message);
 
         // If the SNS record is as a result of an S3 trigger then the message can be converted to an S3EventNotification
-//        Utils.process(S3EventNotification.fromJson(message));
+        // Utils.process(S3EventNotification.fromJson(message));
     }
 
     public static void process(SQSEvent.SQSMessage sqsMessage) {
-        System.out.println("Processing: " + sqsMessage.getBody());
-        System.out.println("Endpoint = " + System.getenv(Constants.PARAM_ENDPOINT));
+        logger.info("Processing SQS message: {}", sqsMessage.getBody());
+        logger.info("Endpoint = {}", System.getenv(Constants.PARAM_ENDPOINT));
     }
 
     public static void process(S3EventNotification s3EventNotification) {
@@ -28,7 +31,6 @@ public class Utils {
     public static void process(S3EventNotification.S3EventNotificationRecord record) {
         String bucket = record.getS3().getBucket().getName();
         String key = record.getS3().getObject().getKey();
-        System.out.println("Processing: " + bucket + " : " + key);
+        logger.info("Processing S3 event: bucket = {}, key = {}", bucket, key);
     }
-
 }
